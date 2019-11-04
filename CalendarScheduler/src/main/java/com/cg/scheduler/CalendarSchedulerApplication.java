ed606cc0795c2f24e559eaa55caa4a9d478d146a
Devang Singh <10.devang@gmail.com>
2019-11-04 10:42:08 +0530

    Added Dummy Content for all

---
+++ CalendarScheduler/src/main/java/com/cg/scheduler/CalendarSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.cg.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarSchedulerApplication.class, args);
+	}
+
+}
---
+++ Scheduler-Angular/README.md
@@ -0,0 +1,4 @@
+# calendar-scheduler-cli
+
+Angular 6 - Calendar Scheduler with Spring Boot and Angular
+
